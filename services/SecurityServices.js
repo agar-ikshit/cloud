26a0ab899e9d8b48a4acbfce7d332fed5abec06e
agar-ikshit <ikshitagarwa@gmail.com>
2025-02-08 00:26:05 +0530

    bug fix

--- services/SecurityServices.js
+++ services/SecurityServices.js
@@ -26,7 +26,7 @@ export const analyzeWebsite = async (req, res) => {
         results.sslInfo = await sslChecker(domain).catch(() => "SSL info not available");
 
         // Puppeteer Analysis
-        const browser = await puppeteer.launch({ headless: true,
+        const browser = await puppeteer.launch({ headless: "new",
             executablePath: process.env.CHROME_PATH || puppeteer.executablePath(),
             args: ["--no-sandbox", "--disable-setuid-sandbox"], });
         const page = await browser.newPage();
